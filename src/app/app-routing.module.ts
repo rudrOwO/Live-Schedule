b5a852bc229ae685ebe1a9a5db7ae6345ca8cf3c
Proma Chow <bsse1132@iit.du.ac.bd>
2022-04-20 22:24:33 +0600

    completed sign up UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component:SignInComponent},
+  {path : 'login' ,component : SignUpComponent},
+  {path : 'signup' ,component : SignInComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor(private router :Router) { }
+
+  ngOnInit(): void {
+  }
+
+  redirect(){
+    this.router.navigate(['signup']);
+  }
+
+}
