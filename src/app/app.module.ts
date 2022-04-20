0a2efdd3cfb5150397e76416e9e8526a2da8a8f0
Dardaul Hoque <rudro1152@gmail.com>
2022-04-21 02:00:20 +0600

    Setup Framework for Schedule and associated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeriodComponent } from './period/period.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { CreatePeriodComponent } from './create-period/create-period.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeriodComponent,
+    ScheduleComponent,
+    CreatePeriodComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/schedule.ts
@@ -0,0 +1,9 @@
+import Period from './period';
+
+export default class Schedule {
+  SUN: Period[] = [];
+  MON: Period[] = [];
+  TUE: Period[] = [];
+  WED: Period[] = [];
+  THU: Period[] = [];
+}
